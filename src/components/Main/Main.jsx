e499b1f6db55063704e4c38e27518bc0e03d23b7
Shihab Shaikh <88572629+shihab-fw11-297@users.noreply.github.com>
2022-05-28 20:29:55 +0530

    Modification Done and Final commit
    Modification Done and Final commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,7 +12,7 @@ export const Main = () => {
   const balloonColor = () => {
     let arr = []
     for (let i = 1; i <= 5; i++) {
-      arr.push([`#${Math.floor(Math.random() * 78605).toString(17)}`, i])
+      arr.push([`#${Math.floor(Math.random() * 16777555).toString(16)}`, i])
     }
     setBalloons(arr)
   }
@@ -25,54 +25,64 @@ export const Main = () => {
   //create function for handaling ballon 
   //for eg. if user click 1st balloon then remove from setadded state 
   //and add to balloon state list
-  const handleBalloon = (e)=>{
-    let newArr = added.filter((elem)=>e[1]!==elem[1])
-    setAdded([...newArr])
-    setBalloons([...balloons, e].sort((a,b)=>a[1]-b[1]))
+  const handleBalloon = (e) => {
+    let newData = added.filter((elem) => e[1] !== elem[1])
+    setAdded([...newData])
+    setBalloons([...balloons, e].sort((a, b) => a[1] - b[1]))
   }
 
   //take a number for ballon and push that ballooon in added list and remove from setballoons
   const HandleShoot = () => {
     // eslint-disable-next-line eqeqeq
-    let balloon = balloons.find((e)=>e[1]==shootvalue)
-    if(!balloon){
-     alert("enter a valid number")
+    let valid = balloons.find((e) => e[1] == shootvalue)
+
+    if (!valid) {
+      alert("Balloon Alredy Added in Box")
       return
     }
+    setShootvalue("")
     // eslint-disable-next-line eqeqeq
-    const newArr = balloons.filter((e)=>e[1]!=shootvalue)
+    const newArr = balloons.filter((e) => e[1] != shootvalue)
+
     setBalloons([...newArr])
-    setAdded([...added, balloon])
+    setAdded([...added, valid])
+
   }
 
   return (
     <div className="container">
 
+      {/* creating box for adding balloon */}
       <div className="box">
-      {added.map((el)=>{
-        return(
-          <div onClick={()=>handleBalloon(el)}>
-             <Balloons color={el[0]} num={el[1]}/>
-          </div>
-        )
-      })}
+        {added.map((el) => {
+          return (
+            <div className="boxBalloon" onClick={() => handleBalloon(el)}>
+              <Balloons key={el[1]} color={el[0]} num={el[1]} />
+            </div>
+          )
+        })}
       </div>
 
+
+      {/* show 5 balloon list*/}
       <div className='balloons-containers'>
         {
           balloons.map((el) =>
-            <Balloons key={el[1]} color={el[0]} num={el[1]}/>
+            <Balloons key={el[1]} color={el[0]} num={el[1]} />
           )}
       </div>
 
+      {/* show inputbox and trigger button*/}
       <div className="textArea">
-        <input
-          type="number"
-          label="enter a number"
-          onChange={(e) => setShootvalue(e.target.value)}
-          className="inputField"
-        />
-        <br />
+        <select className="inputField" value={shootvalue} onChange={(e) => setShootvalue(e.target.value)}>
+          <option defaultValue>Select Baloons</option>
+          <option value="1">1 - First</option>
+          <option value="2">2 - Second</option>
+          <option value="3">3 - Third</option>
+          <option value="4">4 - Fourth</option>
+          <option value="5">5 - Fifth</option>
+        </select>
+
         <button className='shootBtn' onClick={HandleShoot}>Shoot</button>
       </div>
     </div>
